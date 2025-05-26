022c032c98e3e7b0e853faa5d4603ae224cf72f4
mukul9627 <katiyan2906@gmail.com>
2025-05-26 12:11:23 +0530

    Server Changes

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,11 +4,11 @@ const Resume = () => {
   return (
     <div className="flex justify-center">
       <iframe
-          src="https://drive.google.com/file/d/1CSWe-dFQz09CFSiDXsioljsFDtYf6wwo/preview"
-          frameBorder="0"
-          allow="autoplay"
-          className="w-[80%] h-[600px]"
-        ></iframe>
+        src="https://drive.google.com/file/d/1CSWe-dFQz09CFSiDXsioljsFDtYf6wwo/preview"
+        frameBorder="0"
+        allow="autoplay"
+        className="w-[80%] h-[600px]"
+      ></iframe>
     </div>
   );
 };
