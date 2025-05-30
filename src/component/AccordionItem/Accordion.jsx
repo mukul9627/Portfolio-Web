1d9804af726d74a80e424adfacaf13dad15a9257
mukul9627 <katiyan2906@gmail.com>
2025-05-30 11:54:23 +0530

    that today and tomorrow change

--- src/component/AccordionItem/Accordion.jsx
+++ src/component/AccordionItem/Accordion.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import "@/component/AccordionItem/accordion.css";
 import Image from "next/image";
-import Portfolio from "@/myImage/portfolio.png"
+// import { CodeBracketIcon } from '@heroicons/react/24/solid';
+import Portfolio from "@/myImage/portfolio.png";
 
 const AccordionItem = ({ title, content, index, isOpen, toggleOpen }) => (
   <div>
+    {/* <svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  width="24"
+  height="24"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  className="text-gray-800 relative top-[2.8rem]"
+>
+  <polyline points="16 18 22 12 16 6" />
+  <polyline points="8 6 2 12 8 18" />
+</svg> */}
+
     <h2>
       <button
         type="button"
@@ -15,7 +32,51 @@ const AccordionItem = ({ title, content, index, isOpen, toggleOpen }) => (
         aria-expanded={isOpen}
         aria-controls={`accordion-body-${index}`}
       >
-        <span>{title}</span>
+        <span className="flex">
+          <span className="bg-gray-200 rounded-full p-2 flex items-center justify-center w-[50px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="text-gray-800"
+            >
+              <polyline points="16 18 22 12 16 6" />
+              <polyline points="8 6 2 12 8 18" />
+            </svg>
+          </span>{" "}
+          <span className="pl-2">
+            {title}
+            <span>
+              <p className="text-sm text-muted-foreground flex items-center mt-1">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="h-4 w-4 mr-1"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
+                </svg>
+                01/08/2024
+              </p>
+            </span>
+          </span>
+        </span>
+
         <svg
           data-accordion-icon
           className={`w-3 h-3 shrink-0 transition-transform duration-200 ${
@@ -53,10 +114,14 @@ const Accordion = () => {
   };
   const items = [
     {
-      title: "What is Flowbite?",
+      title: "Portfolio",
       content: (
         <>
-        <Image src={Portfolio} alt="project Img" className="mb-4 w-30% h-30% ml-[12rem]"/>
+          <Image
+            src={Portfolio}
+            alt="project Img"
+            className="mb-4 w-30% h-30% ml-[12rem]"
+          />
           <p className="mb-2">
             Flowbite is an open-source library of interactive components built
             on top of Tailwind CSS including buttons, dropdowns, modals,
